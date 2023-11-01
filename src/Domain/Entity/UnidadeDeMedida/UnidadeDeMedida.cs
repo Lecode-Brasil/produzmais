1eca6656543905fe5e86ef7eabd30d296b372a37
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2023-11-01 18:52:11 -0300

    UnidadeDeMedida instantiate

---
+++ src/Domain/Entity/UnidadeDeMedida/UnidadeDeMedida.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entity.UnidadeDeMedida;
+
+public class UnidadeDeMedida
+{
+    public string Abreviacao { get; }
+    public string Descricao { get; }
+
+    public UnidadeDeMedida(string abreviacao, string descricao)
+    {
+        Abreviacao = abreviacao;
+        Descricao = descricao;
+    }
+}
---
+++ tests/Domain.UnitTests/Entity/UnidadeDeMedida/UnidadeDeMedidaTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.UnitTests.Entity.UnidadeDeMedida;
+
+public class UnidadeDeMedidaTest
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    public void Instantiate()
+    {
+        var validData = new
+        {
+            Abreviacao = "PC",
+            Descricao = "Peça",
+        };
+
+        Domain.Entity.UnidadeDeMedida.UnidadeDeMedida unidadeDeMedida = new(validData.Abreviacao, validData.Descricao);
+
+        Assert.NotNull(unidadeDeMedida);
+        Assert.Equal(validData.Abreviacao, unidadeDeMedida.Abreviacao);
+        Assert.Equal(validData.Descricao, unidadeDeMedida.Descricao);
+    }
+}
