462fcf690e4f80a7097faf7caa9046de6036b99c
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2023-12-10 19:31:23 -0300

    Refactoring do teste

--- tests/Domain.UnitTests/Application/CreateUnidadeDeMedida/CreateUnidadeDeMedidaTest.cs
+++ tests/Domain.UnitTests/Application/CreateUnidadeDeMedida/CreateUnidadeDeMedidaTest.cs
@@ -1,25 +1,28 @@
-﻿using Application.Interfaces;
-using Domain.Entity.UnidadeDeMedida;
-using Domain.Repository;
+﻿using Domain.Entity.UnidadeDeMedida;
 using FluentAssertions;
 using Moq;
 using UseCases = Application.UseCases.UnidadeDeMedida.CreateUnidadeDeMedida;
 
 namespace UnitTests.Application.CreateUnidadeDeMedida;
 
+[Collection(nameof(CreateUnidadeDeMedidaTestFixture))]
 public class CreateUnidadeDeMedidaTest
 {
+    private readonly CreateUnidadeDeMedidaTestFixture _fixture;
+
+    public CreateUnidadeDeMedidaTest(CreateUnidadeDeMedidaTestFixture fixture) => _fixture = fixture;
+
     [Fact]
     public async Task CreateUnidadeDeMedida()
     {
-        var repositoryMock = new Mock<IUnidadeDeMedidaRepository>();
-        var unitOfWorkMock = new Mock<IUnitOfWork>();
+        var repositoryMock = _fixture.GetUnidadeDeMedidaRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
         var useCase = new UseCases.CreateUnidadeDeMedida(
             repositoryMock.Object,
             unitOfWorkMock.Object);
         UseCases.CreateUnidadeDeMedidaInput input = new(
-            Abreviacao: "foo",
-            Descricao: "bar",
+            Abreviacao: _fixture.GetValidUnidadeDeMedidaAbreviacao(),
+            Descricao: _fixture.GetValidUnidadeDeMedidaDescricao(),
             Ativo: true);
 
         UseCases.CreateUnidadeDeMedidaOutput output = await useCase.Handle(input, CancellationToken.None);
@@ -30,9 +33,7 @@ public class CreateUnidadeDeMedidaTest
                 It.IsAny<CancellationToken>()
             ),
             Times.Once);
-        unitOfWorkMock.Verify(
-            uow => uow.Commit(It.IsAny<CancellationToken>()),
-            Times.Once);
+        unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Once);
         output.Should().NotBeNull();
         output.Abreviacao.Should().Be(input.Abreviacao);
         output.Descricao.Should().Be(input.Descricao);
---
+++ tests/Domain.UnitTests/Application/CreateUnidadeDeMedida/CreateUnidadeDeMedidaTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using Application.Interfaces;
+using Domain.Repository;
+using Moq;
+using UnitTests.Common;
+using DomainEntity = Domain.Entity.UnidadeDeMedida;
+
+namespace UnitTests.Application.CreateUnidadeDeMedida;
+
+[CollectionDefinition(nameof(CreateUnidadeDeMedidaTestFixture))]
+public class CreateUnidadeDeMedidaTestFixtureCollection
+    : ICollectionFixture<CreateUnidadeDeMedidaTestFixture>
+{
+}
+
+public class CreateUnidadeDeMedidaTestFixture
+    : BaseFixture
+{
+    public string GetValidUnidadeDeMedidaAbreviacao()
+    {
+        var abreviacao = Faker.Commerce.Categories(1).First();
+        return abreviacao[..Math.Min(DomainEntity.UnidadeDeMedida.AbreviacaoMaxLength, abreviacao.Length)];
+    }
+
+    public string GetValidUnidadeDeMedidaDescricao()
+    {
+        var descricao = Faker.Commerce.Product();
+        return descricao[..Math.Min(DomainEntity.UnidadeDeMedida.DescricaoMaxLength, descricao.Length)];
+    }
+
+    public Mock<IUnidadeDeMedidaRepository> GetUnidadeDeMedidaRepositoryMock() => new();
+
+    public Mock<IUnitOfWork> GetUnitOfWorkMock() => new();
+
+}
